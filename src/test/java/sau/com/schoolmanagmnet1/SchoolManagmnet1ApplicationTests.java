dde33a60a6acb72ac4f484d57ca1d64fcdbaaeda
yasinsblk <yasinsebelek61@gmail.com>
2026-03-08 16:13:30 +0300

    first commit

---
+++ src/test/java/sau/com/schoolmanagmnet1/SchoolManagmnet1ApplicationTests.java
@@ -0,0 +1,13 @@
+package sau.com.schoolmanagmnet1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagmnet1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
